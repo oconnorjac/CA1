699cc02043cc523daa9106191cfeba4cb9c993f6
jacqueline <D00233669@student.dkit.ie>
2020-10-21 22:16:25 +0100

    fourth commit.

--- src/main/java/com/dkit/sd2b/jacquelineoconnor/App.java
+++ src/main/java/com/dkit/sd2b/jacquelineoconnor/App.java
@@ -3,46 +3,46 @@ package com.dkit.sd2b.jacquelineoconnor;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
 
+        //variables
         File inputFile = new File("JC_Results.txt");
-        readFile(inputFile);
+        int[] codes = new int[8];
+        int[] grades = new int[8];
 
-    }
-
-    public static void readFile(File inputFile){
-
-        //int[] codes = new int[8];
-        //int[] grades = new int[8];
         //double averageGrade = 0;
-
 
         try {
             Scanner input = new Scanner(inputFile);
             input.useDelimiter("[,\r\n]+");
 
             while (input.hasNext()) {
-                String student = input.nextLine();
-                System.out.println(student);
+
+                int studentNumber = input.nextInt();
+                for (int i = 0; i < 8; i++) {
+                    codes[i] = input.nextInt();
+                    grades[i] = input.nextInt();
+                }
+                selectFiveGrades(codes, grades);
+                //System.out.println(calculateAverage(selectedGrades));
+
+                //testing
+                System.out.println(studentNumber);
+                System.out.println(Arrays.toString(codes));
+                System.out.println(Arrays.toString(grades));
             }
             input.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-
-        //variables used to store grades when finding 2 largest
-        //int largestGrade = 0;
-        //int secondLargest = 0;
-        //int thirdLargest = 0; //in case of CSPE code=218
-        //largestGrade = Math.max(largestGrade, studentData[i+1]);
-
     }
 
-    public int[] selectFiveGrades(int[] codes, int[] grades) {
+    public static int[] selectFiveGrades(int[] codes, int[] grades) {
 
         // to accept the eight available codes and corresponding grades, and return an array of the
         //five selected grades (based on the selection algorithm)
@@ -62,15 +62,29 @@ public class App {
             }
         }
 
+        //testing
+        System.out.println(Arrays.toString(selectedGrades));
+
+
+        //variables used to store grades when finding 2 largest
+        //int largestGrade = 0;
+        //int secondLargest = 0;
+        //int thirdLargest = 0; //in case of CSPE code=218
+        //largestGrade = Math.max(largestGrade, studentData[i+1]);
 
         return selectedGrades;
 
     }
 
     private double calculateAverage(int[] selectedGrades) {
-        // to calculate the average
 
-        return 0;
+        // to calculate the average
+        int sum = 0;
+        for (int grade : selectedGrades) {
+            sum += grade;
+        }
+
+        return sum / 5;
     }
 
 }
